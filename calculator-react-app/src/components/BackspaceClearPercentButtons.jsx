d69c0620a98111dece1de8626dc90107fbc22220
courtneytruong <courtneybtruong@gmail.com>
2026-08-07 13:21:52 -0400

    Numbers buttons placed, color tweaks.

--- calculator-react-app/src/components/BackspaceClearPercentButtons.jsx
+++ calculator-react-app/src/components/BackspaceClearPercentButtons.jsx
@@ -1,7 +1,7 @@
 function BackspaceClearPercentButtons({ BackspaceClearPercentButton }) {
   return (
     <div className="flex h-1/3 w-full text-3xl font-bold text-white">
-      <button className="bg-gray-400 border-1 border-gray-200 hover:bg-gray-500 items-center justify-center w-[65px] h-[65px] rounded-full">
+      <button className="bg-zinc-400 border-1 border-zinc-200 hover:bg-zinc-500 items-center justify-center w-[65px] h-[65px] rounded-full">
         {BackspaceClearPercentButton}
       </button>
     </div>
--- calculator-react-app/src/components/NumbersDecimalPointContainer.jsx
+++ calculator-react-app/src/components/NumbersDecimalPointContainer.jsx
@@ -1,7 +1,31 @@
+import NumbersDecimalPointButtons from "./NumbersDecimalPointButtons";
+
+const numbersDecimalPoint = [
+  "7",
+  "8",
+  "9",
+  "4",
+  "5",
+  "6",
+  "1",
+  "2",
+  "3",
+  "+/-",
+  "0",
+  ".",
+];
+
 function NumbersDecimalPointContainer() {
   return (
-    <div className="flex bg-slate-500 w-full h-5/6">
-      Numbers and Decimal Point Container
+    <div className="w-full h-1/6">
+      <div className="grid grid-cols-3 gap-2 justify-center w-full h-full">
+        {numbersDecimalPoint.map((number) => (
+          <NumbersDecimalPointButtons
+            key={number}
+            number={number}
+          ></NumbersDecimalPointButtons>
+        ))}
+      </div>
     </div>
   );
 }
