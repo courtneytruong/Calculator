a7e82001d8a08d02505d220fd5037f6042045bcb
courtneytruong <courtneybtruong@gmail.com>
2026-08-07 15:33:13 -0400

    removed unnecessary flex elements in buttons

--- calculator-react-app/src/components/NumbersDecimalPointButtons.jsx
+++ calculator-react-app/src/components/NumbersDecimalPointButtons.jsx
@@ -1,7 +1,7 @@
 function NumbersDecimalPointButtons({ number }) {
   return (
     <div className="flex h-5/6 w-full text-3xl font-bold text-white">
-      <button className="bg-zinc-600 border-1 border-zinc-400 hover:bg-zinc-700 items-center justify-center w-[65px] h-[65px] rounded-full">
+      <button className="bg-zinc-600 border-1 border-zinc-400 hover:bg-zinc-700 w-[65px] h-[65px] rounded-full">
         {number}
       </button>
     </div>
--- calculator-react-app/src/components/OperatorButton.jsx
+++ calculator-react-app/src/components/OperatorButton.jsx
@@ -1,7 +1,7 @@
 function OperatorButton({ operator }) {
   return (
     <div className="flex justify-center items-center h-1/6 w-full text-3xl font-bold text-white">
-      <button className="bg-orange-400 border-1 border-orange-200 hover:bg-orange-500 items-center justify-center w-[65px] h-[65px] rounded-full">
+      <button className="bg-orange-400 border-1 border-orange-200 hover:bg-orange-500 w-[65px] h-[65px] rounded-full">
         {operator}
       </button>
     </div>
